10b71d5ee21c154aeeb2f18d190d0762b701dab5
Bad3r <github@unsigned.sh>
2026-08-13 13:31:43 +0300

    chore: update mpv scripts

--- pkgs/by-name/mp/mpv/scripts/mpvacious.nix
+++ pkgs/by-name/mp/mpv/scripts/mpvacious.nix
@@ -10,20 +10,18 @@
 
 buildLua rec {
   pname = "mpvacious";
-  version = "26.1.26.0";
+  version = "26.7.28.0";
 
   src = fetchFromGitHub {
     owner = "Ajatt-Tools";
     repo = "mpvacious";
     rev = "v${version}";
-    sha256 = "sha256-I0h9tf1pwCkWPn/6t2mxhe4MPx6GsPR+F5F29NAgdXI=";
+    sha256 = "sha256-FZvjaIIsU0LNXTJrVJWOhbN2oth9vBcP1vQf4JrXPXg=";
   };
   passthru.updateScript = gitUpdater { rev-prefix = "v"; };
 
   postPatch = ''
-    substituteInPlace utils/forvo.lua \
-      --replace-fail "'curl" "'${lib.getExe curl}"
-    substituteInPlace platform/nix.lua \
+    substituteInPlace mpvacious/platform/nix.lua \
       --replace-fail "'curl" "'${lib.getExe curl}" \
       --replace-fail "'wl-copy" "'${lib.getExe' wl-clipboard "wl-copy"}" \
       --replace-fail "'xclip" "'${lib.getExe xclip}"
@@ -31,7 +29,7 @@ buildLua rec {
 
   installPhase = ''
     runHook preInstall
-    make PREFIX=$out/share/mpv install
+    make PREFIX=$out/share/mpv VERSION=${version} install
     runHook postInstall
   '';
 
